1621ad89517d6cffd279e648bedb732b8690274d
Jessica Barnett <jlbarn11@asu.edu>
2023-05-11 21:00:58 -0700

    local JSON access working
    Fetch of local JSON working. Additional content in main, adjustments to layout.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -68,22 +68,44 @@ $(function(){
 
 
     // get the data stored in our local JSON file so we can use it to display information to the user and create the major map builder
-    // $.getJSON("scripts/courses.json", parseJSONfile(data));
-//     $.getJSON("scripts/courses.json")
-//     .done(console.log(data))
-//     .fail(console.log("I have no idea what is happening"));
 
     fetch("scripts/courses.json")
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then(data => handleJSONfile(data));
 
     // variables to store the different types of data in our JSOn file to be used in different sections of the page
     let focusAreas, coreGitCourses, coreNonGitCourses, genStudies, gitElectives, electives, focusAreaCourses, photoCourses, printCourses, videoCourses, frontEndCourses, interactionDesignCourses, motionGraphicsCourses, uxCourses;
 
+    // Object to represent a major map for campus programs, to be filled with data from our JSON as needed
+    let majorMapCampus = {
+        "terms": [
+            {"name": 1, "courses": []}, {"name": 2, "courses": []}, {"name": 3, "courses": []}, {"name": 4, "courses": []}, {"name": 5, "courses": []}, {"name": 6, "courses": []}, {"name": 7, "courses": []}, {"name": 8, "courses": []}
+        ]
+    };
+
+    // Object to represent a major map for online programs, to be filled with data from our JSON as needed
+    let majorMapOnline = {
+        "terms": [
+            {"name": "1A", "courses": []}, {"name": "1B", "courses": []}, {"name": "2A", "courses": []}, {"name": "2B", "courses": []}, {"name": "3A", "courses": []}, {"name": "3B", "courses": []}, {"name": "4A", "courses": []}, {"name": "4B", "courses": []}, {"name": "5A", "courses": []}, {"name": "5B", "courses": []}, {"name": "6A", "courses": []}, {"name": "6B", "courses": []}, {"name": "7A", "courses": []}, {"name": "7B", "courses": []}, {"name": "8A", "courses": []}, {"name": "8B", "courses": []}
+        ]
+    };
+
     // Function to handle parsing the JSON file of courses for display
-    
-    function parseJSONfile(json){
-        let data = $.parseJSON(json);
-        console.log('json data in parseJSONfile function', data);
+    function handleJSONfile(json){
+        // add the different parts of the JSON to the variables we created above
+        focusAreas = json.focusAreas;
+        coreGitCourses = json.coreGit;
+        coreNonGitCourses = json.coreNonGit;
+        genStudies = json.generalStudies;
+        gitElectives = json.gitElectives;
+        electives = json.electives;
+        focusAreaCourses = json.focusAreaCourses;
+        photoCourses = json.commercialPhoto;
+        printCourses = json.commercialPrint;
+        videoCourses = json.commercialVideo;
+        frontEndCourses = json.frontEndWebDev;
+        interactionDesignCourses = json.interactionDesign;
+        motionGraphicsCourses = json.motionGraphics;
+        uxCourses = json.userExperience;
     }
 });
